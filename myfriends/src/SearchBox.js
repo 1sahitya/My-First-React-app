f758590a2c496d98f9d8aab2efc1dbe7f190828c
11sahitya@gmail.com <11sahitya@gmail.com>
2019-05-22 16:18:51 +0530

    added a new Search features

---
+++ myfriends/src/SearchBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchBox = ({ searchfield, searchChange }) => {
+    return (
+        <div className='pa2'>
+
+         <input 
+         className='pa3 ba g--green bg-lightest-blue'
+         type='seach' 
+         placeholder='search friends'
+         onChange={searchChange} 
+         />
+
+        </div>
+        
+    );
+}
+
+export default SearchBox;
